516c85b07b1fab49fe2b1d1e42dce8dabe0b1694
Ashley Richardson <ashley-richardson@lambdastudents.com>
2020-08-13 15:27:39 -0400

    npm

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
@@ -58,3 +59,4 @@ const followersArray = [];
     luishrd
     bigknell
 */
+
